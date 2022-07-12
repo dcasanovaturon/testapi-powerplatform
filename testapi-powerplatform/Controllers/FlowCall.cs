9c478dc901db870c1276e6fab3a0b8310a2a682b
David Casanova Turón <dcasanovaturon@users.noreply.github.com>
2022-07-12 18:08:40 +0200

    Api
    Api

---
+++ testapi-powerplatform/Controllers/FlowCall.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testapi_powerplatform.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FlowCall : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public FlowCall(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<FlowCallRet> Get()
+        {
+            var remoteIpAddress = HttpContext.Connection.RemoteIpAddress;
+
+            return Enumerable.Range(1, 1).Select(index => new FlowCallRet
+            {
+                URLOri = remoteIpAddress.ToString()
+            }).ToArray();
+        }
+    }
+}
---
+++ testapi-powerplatform/FlowCallRet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testapi_powerplatform
+{
+    public class FlowCallRet
+    {
+        public string URLOri { get; set; }
+    }
+}
